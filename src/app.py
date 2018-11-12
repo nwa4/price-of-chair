0408ae1d243cc4ef82b8d970aa4df4e7d9ef873b
nwa4 <obi@nwa4.com>
2018-11-12 06:33:14 +0200

    Edited app start

--- src/app.py
+++ src/app.py
@@ -24,3 +24,6 @@ from src.models.alerts.views import alert_blueprint
 app.register_blueprint(user_blueprint, url_prefix="/users")
 app.register_blueprint(store_blueprint, url_prefix="/stores")
 app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+
+
+#app.run()
